78bd3e6a2a2739176ba19393a4c68d47c5863442
Balaji Rao <balaji@poornaprajna.org>
2010-08-31 11:15:11 +0530

    Initial commit
    Signed-off-by: Balaji Rao <balaji@poornaprajna.org>

---
+++ density_matrix.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import numpy as np
+import itertools as it
+
+# Index of |ij> <kl|
+def rho_idx(i, j, k, l, dim_A = 2, dim_B = 2) :
+	return (i * dim_B + j, k * dim_B + l)
+
+def partial_trace(rho_AB, subsys = 2, dim_A = 2, dim_B = 2) :
+	d = dim_A * dim_B
+	if (rho_AB.shape != (d, d)) :
+		print "Bad Matrix"
+		raise Exception('Bad Matrix')
+	
+	if (subsys == 2) :
+		dim_reduced = dim_A
+	else:
+		dim_reduced = dim_B
+
+	rho_reduced = np.zeros((dim_reduced, dim_reduced))
+
+#	idxes = it.product(range(dim_A), repeat=2) # New in 2.6
+	idxes = ((x,y) for x in range(dim_reduced) for y in range(dim_reduced))
+
+	for idx in idxes :
+		for i in range(d / dim_reduced) :
+			x, y = idx
+			if (subsys == 1):
+				rho_reduced[idx] += rho_AB[rho_idx(i, x, i, y, dim_A, dim_B)]
+			else:
+				rho_reduced[idx] += rho_AB[rho_idx(x, i, y, i, dim_A, dim_B)]
+	
+	return np.asmatrix(rho_reduced)
+
+def log2(x):
+	if (x == 0):
+		return 0
+	else :
+		return np.log2(x)
+
+def H(ev):
+	return -ev * log2(ev)
+
+def S(rho) :
+	s = 0
+	evals = np.round(np.linalg.eigvals(rho), 5)
+	for ev in evals :
+		s += H(ev)
+	return s
+
+def S_cond(rho_AB, cond = (1, 2), dim_A = 2, dim_B = 2) :
+	return S(rho_AB) - S(partial_trace(rho_AB, cond[1], dim_A, dim_B))
+
---
+++ discord.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+import numpy as np
+import density_matrix
+import matplotlib.pyplot as plt
+
+res = 25
+
+def projector(v) :
+	return v * v.H
+
+def T(rho_AB, theta, phi, dim_A = 2, dim_B = 2, qubit_subsys = 2) :
+	_basis = [None for i in range(2)]
+	_x = np.cos (theta)
+	_y = np.sin (theta)
+	_basis[0] = np.mat([[_x], [np.exp(1j * phi) * _y]])
+	_basis[1] = np.mat([[_y], [-np.exp(1j * phi) * _x]])
+
+	t = 0
+
+	for i in range(len(_basis)):
+		if (qubit_subsys == 1) :
+			_op = np.kron(projector(_basis[i]), np.eye(dim_B))
+		else:
+			_op = np.kron(np.eye(dim_A), projector(_basis[i]))
+
+		_rho = _op * rho_AB
+		#_rho = _op * rho_AB * _op
+
+		p = np.trace(_rho).real
+
+		_rho_reduced = density_matrix.partial_trace(_rho, qubit_subsys % 2 + 1, dim_A = dim_A, dim_B = dim_B) / p
+		t += p * density_matrix.S(_rho_reduced)
+
+	return t
+
+def T_min(rho_AB, dim_A = 2, dim_B = 2, qubit_subsys = 2) :
+	tp = ((theta, phi) for theta in np.linspace(0, 2 * np.pi, res) for phi in np.linspace(0, np.pi, res))
+
+	t_min = np.inf
+	for t, p in tp:
+		t = T(rho_AB, t, p, dim_A = dim_A, dim_B = dim_B, qubit_subsys = qubit_subsys)
+		if (t < t_min):
+			t_min = t
+	return t_min
+
+
+
