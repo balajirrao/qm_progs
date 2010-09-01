aeb4eede709932f133bcef7b869191f414738cb5
Balaji Rao <balaji@poornaprajna.org>
2010-09-01 13:14:15 +0530

    Plot QD for various values of noise parameter
    Signed-off-by: Balaji Rao <balaji@poornaprajna.org>

--- random_walk/lib_based/line_amp_damp.py
+++ random_walk/lib_based/line_amp_damp.py
@@ -7,6 +7,7 @@ import density_matrix as dm
 import discord
 import sys
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Graphical output needed ?
 graph = True
@@ -17,7 +18,6 @@ print_probs = False
 steps = int(sys.argv[1])
 
 # Parameter for the Kraus operators
-lamda = 0.15
 
 #Inludes 3..
 
@@ -25,17 +25,29 @@ pos_list = range (-steps, steps + 1)
 pos_space = qwalk.PositionSpace(pos_list)
 coin_space = qwalk.CoinSpace()
 
-E = [np.mat(np.empty((2 * (2 * steps + 1), 2 * (2 * steps + 1)))) for i in range (2)]
-E[0] = np.kron(np.mat([[ np.sqrt(1 - lamda), 0], [0, 1]]), pos_space.I)
-E[1] = np.kron(np.mat([[0, 0], [np.sqrt(lamda), 0]]), pos_space.I)
+values = list()
 
-walk = qwalk.QWalk(pos_space, coin_space)
-rho_CP = walk.do(steps, E)
+lamda_space = np.linspace(0, 0.9, 10)
+
+for lamda in lamda_space :
+	E = [np.mat(np.empty((2 * (2 * steps + 1), 2 * (2 * steps + 1)))) for i in range (2)]
+	E[0] = np.kron(np.mat([[ np.sqrt(1 - lamda), 0], [0, 1]]), pos_space.I)
+	E[1] = np.kron(np.mat([[0, 0], [np.sqrt(lamda), 0]]), pos_space.I)
+
+	walk = qwalk.QWalk(pos_space, coin_space)
+	rho_CP = walk.do(steps, E)
 
 #rho_P = dm.partial_trace(rho_CP, subsys = 1, dim_B = len(pos_space), dim_A = len(coin_space))
 #print np.diag(rho_P), np.trace(rho_P)
 
-dim_A = len(coin_space)
-dim_B = len(pos_space)
+	dim_A = len(coin_space)
+	dim_B = len(pos_space)
 
-print discord.T_min(rho_CP, dim_A, dim_B, qubit_subsys = 1) - dm.S_cond(rho_CP, (1, 2), dim_A, dim_B)
+	_v = discord.T_min(rho_CP, dim_A, dim_B, qubit_subsys = 1) - dm.S_cond(rho_CP, (2, 1), dim_A, dim_B)
+	values.append(_v)
+
+
+plt.plot(lamda_space, values)
+plt.xlabel('Noise parameter')
+plt.ylabel('D')
+plt.show()
