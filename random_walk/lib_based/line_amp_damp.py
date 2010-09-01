c37279c1b8e17515034a9febc2b95d45fc578aa1
Balaji Rao <balaji@poornaprajna.org>
2010-09-01 16:57:57 +0530

    Add capability to run the line_amp_damp script for a range of steps.
    Signed-off-by: Balaji Rao <balaji@poornaprajna.org>

--- random_walk/lib_based/line_amp_damp.py
+++ random_walk/lib_based/line_amp_damp.py
@@ -15,39 +15,56 @@ graph = True
 # Print the probs ?
 print_probs = False
 
-steps = int(sys.argv[1])
+step_spec = sys.argv[1].split(':')
 
-# Parameter for the Kraus operators
+step_start = step_end = step_steps = 0
 
-#Inludes 3..
+if (len(step_spec) == 0 or len(step_spec) > 3) :
+	print "Bad args!"
+	exit()
+else :
+	if len(step_spec) >= 1 :
+		step_start = int(step_spec[0])
+		step_end = step_start
+		step_steps = 1
+		
+	if len(step_spec) >= 2 :
+		step_end = int(step_spec[1])
+		step_steps = 10
 
-pos_list = range (-steps, steps + 1)
-pos_space = qwalk.PositionSpace(pos_list)
+	if len(step_spec) == 3 :
+		step_steps = int(step_spec[2])
+
+	#Noise parameter
+	lamda = 0
+
+#Coinspace is invariant
 coin_space = qwalk.CoinSpace()
+dim_A = len(coin_space)
 
 values = list()
+steps_space = range(step_start, step_end, step_steps)
+for steps in steps_space:
+	pos_list = range (-steps, steps + 1)
+	pos_space = qwalk.PositionSpace(pos_list)
+	dim_B = len(pos_space)
 
-lamda_space = np.linspace(0, 0.9, 10)
-
-for lamda in lamda_space :
 	E = [np.mat(np.empty((2 * (2 * steps + 1), 2 * (2 * steps + 1)))) for i in range (2)]
 	E[0] = np.kron(np.mat([[ np.sqrt(1 - lamda), 0], [0, 1]]), pos_space.I)
 	E[1] = np.kron(np.mat([[0, 0], [np.sqrt(lamda), 0]]), pos_space.I)
 
 	walk = qwalk.QWalk(pos_space, coin_space)
+
 	rho_CP = walk.do(steps, E)
+	_v = discord.T_min(rho_CP, dim_A, dim_B, qubit_subsys = 1) - dm.S_cond(rho_CP, (2, 1), dim_A, dim_B)
+	values.append(_v)
+
+	print steps, " : ", _v
 
 #rho_P = dm.partial_trace(rho_CP, subsys = 1, dim_B = len(pos_space), dim_A = len(coin_space))
 #print np.diag(rho_P), np.trace(rho_P)
 
-	dim_A = len(coin_space)
-	dim_B = len(pos_space)
-
-	_v = discord.T_min(rho_CP, dim_A, dim_B, qubit_subsys = 1) - dm.S_cond(rho_CP, (2, 1), dim_A, dim_B)
-	values.append(_v)
-
-
-plt.plot(lamda_space, values)
-plt.xlabel('Noise parameter')
+plt.plot(steps_space, values)
+plt.xlabel('Steps')
 plt.ylabel('D')
 plt.show()
