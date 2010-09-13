8e73b54058ad2702d7173a3828267559e784c922
Balaji Rao <balaji@poornaprajna.org>
2010-09-13 23:18:03 +0530

    Enhance circular random quantum walk to work on range of traversals
    Signed-off-by: Balaji Rao <balaji@poornaprajna.org>

--- random_walk/lib_based/circle.py
+++ random_walk/lib_based/circle.py
@@ -17,12 +17,13 @@ parser.add_option('-g', '--graphical', action = 'store_true', help = 'show graph
 parser.add_option('-v', '--verbose', help = 'verbose output ', type = int, default = 1, dest = 'verbose', metavar = "LEVEL")
 
 parser.add_option('-N', '--node-range', type = 'int',  nargs = 3, help = 'Range of nodes to use', dest = 'node_range', metavar = "START END RESOLUTION")
-parser.add_option('-n', '--nodes', type = 'int',  help = 'Number of nodes', dest = 'nodes', metavar = "NODES")
+parser.add_option('-n', '--nodes', type = 'int',  default = 25, help = 'Number of nodes', dest = 'nodes', metavar = "NODES")
 
 parser.add_option('-L', '--lamda-range', type = 'float',  nargs = 3, help = 'Range of lamdas to use for amp damping', dest = 'lamda_range', metavar = "START END RESOLUTION")
-parser.add_option('-l', '--lamda', type = 'float',  help = 'Lamda param for amp damping', dest = 'lamda', metavar = "LAMDA")
+parser.add_option('-l', '--lamda', type = 'float',  default = 0, help = 'Lamda param for amp damping', dest = 'lamda', metavar = "LAMDA")
 
-parser.add_option('-t', '--traversals', type = 'int',  default = 1, help = 'Number of times to traverse the circle', dest = 'traversals', metavar = "N")
+parser.add_option('-t', '--traversals', type = 'int',  default = 5, help = 'Number of times to traverse the circle', dest = 'traversals', metavar = "N")
+parser.add_option('-T', '--traversals-range', type = 'int',  nargs = 3, help = 'Number of times to traverse the circle', dest = 'traversals_range', metavar = "N")
 
 (options, args) = parser.parse_args()
 
@@ -35,25 +36,32 @@ dim_A = len(coin_space)
 
 values = list()
 
-if (options.node_range != None) :
+_options = 0
+
+nodes_space = lamda_space = traversals_space = None
+
+if options.node_range is not None :
 	nodes_space = range(*options.node_range)
-
-	if options.lamda_range is not None :
-		lamda_space = [options.lamda_range[0]]
-		print "Can't use --lamda-range with --node-range. Using single lamda ",lamda_space[0]
-	else :
-		if options.lamda is not None :
-			lamda_space = [options.lamda]
-		else:
-			lamda_space = [0]
-			print "Value of lamda not given. Using 0"
-
-else :
+	_options += 1
+else:
 	nodes_space = [options.nodes]
-	
-	if options.lamda_range is None :
-		lamda_space = np.linspace(0.1, 1, 10)
-		print "--lamda-range not provided. Using ", lamda_space
+
+
+if options.lamda_range is not None :
+	lamda_space = range(*options.lamda_range)
+	_options += 1
+else:
+	lamda_space = [options.lamda]
+
+if options.traversals_range is not None :
+	traversals_space = range(*options.traversals_range)
+	_options += 1
+else:
+	traversals_space = [options.traversals]
+
+if _options == 0 or _options != 1 :
+	parser.print_help()
+	sys.exit(1)
 
 for nodes in nodes_space:
 	if (nodes % 2 == 0) :
@@ -66,32 +74,41 @@ for nodes in nodes_space:
 
 	walk = qwalk.QWalk(pos_space, coin_space, circle = True)
 
-
 	for lamda in lamda_space:
 		E = [np.mat(np.empty((2 * (2 * nodes + 1), 2 * (2 * nodes + 1)))) for i in range (2)]
 		E[0] = np.kron(np.mat([[ np.sqrt(1 - lamda), 0], [0, 1]]), pos_space.I)
 		E[1] = np.kron(np.mat([[0, 0], [np.sqrt(lamda), 0]]), pos_space.I)
 
-		rho_CP = walk.do(options.traversals * nodes, E)
+		for traversals in traversals_space :
+			rho_CP = walk.do(traversals * nodes, E)
+			_qd = discord.T_min(rho_CP, dim_A, dim_B, qubit_subsys = 1) - dm.S_cond(rho_CP, (2, 1), dim_A, dim_B).real
+			values.append(_qd)
 
-		_qd = discord.T_min(rho_CP, dim_A, dim_B, qubit_subsys = 1) - dm.S_cond(rho_CP, (2, 1), dim_A, dim_B).real
-		values.append(_qd)
-
-		if options.verbose > 0 :
-			print "Nodes : ", nodes, " Lambda = ", lamda
-			print "\tQD = ", _qd
+			if options.verbose > 0 :
+				print "Nodes : ", nodes, " Lambda = ", lamda, 'Traversals = ', traversals
+				print "\tQD = ", _qd
 			
-			if options.verbose > 1 :
-				rho_P = dm.partial_trace(rho_CP, subsys = 1, dim_B = len(pos_space), dim_A = len(coin_space))
-				print "\tProbabilities : ", np.diag(rho_P), ". Sum = ", np.trace(rho_P)
+				if options.verbose > 1 :
+					rho_P = dm.partial_trace(rho_CP, subsys = 1, dim_B = len(pos_space), dim_A = len(coin_space))
+					print "\tProbabilities : ", np.diag(rho_P), ". Sum = ", np.trace(rho_P)
 
 if (options.graphical) :
 	if options.node_range is not None :
-		plt.plot(nodes_space, values)
-		plt.xlabel('Steps')
+		_x = nodes_space
+		_label = 'Nodes'
+		_title = 'Walk on circle with lamda = ' + str(options.lamda) + ', traversals = ' + str(options.traversals)
+	elif options.lamda_range is not None :
+		_x = lamda_space
+		_label = 'Noise Parameter'
+		_title = 'Walk on circle with nodes = '+ str(options.nodes) + ', traversals = ' + str(options.traversals)
 	else :
-		plt.plot(lamda_space, values)
-		plt.xlabel('Noise Parameter')
+		_x = traversals_space
+		_label = 'Traversals'
+		_title = 'Walk on circle with nodes = ' + str(options.nodes) + ', lamda = ' + str(options.lamda)
+
+	plt.plot(_x, values)
+	plt.xlabel(_label)
+	plt.title(_title)
 
 	plt.ylabel('D')
 	plt.show()
