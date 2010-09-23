512abef5b1351344e4979f858a8b5f4b7820a9d1
Balaji Rao <balaji@poornaprajna.org>
2010-09-23 09:30:19 +0530

    Add support for calculating quantumness from measurement induced disturbance - qmid
    Signed-off-by: Balaji Rao <balaji@poornaprajna.org>

---
+++ lib/qmid.py
@@ -0,0 +1,24 @@
+import numpy as np
+import density_matrix
+import matplotlib.pyplot as plt
+
+def proj(v) :
+	return v * v.H
+
+def qmid(rho, dim_A = 2, dim_B = 2) :
+	rho_A = density_matrix.partial_trace(rho, subsys = 2)
+	rho_B = density_matrix.partial_trace(rho, subsys = 1)
+
+	val_A, vec_A = np.linalg.eig(rho_A)
+	val_B, vec_B = np.linalg.eig(rho_B)
+
+	pi = list()
+	for v_A in vec_A :
+		for v_B in vec_B:
+			pi.append(np.kron(proj(v_A.T), proj(v_B.T)))
+
+	_rho = np.zeros_like(rho)
+	for p in pi :
+		_rho += p * rho * p
+
+	return density_matrix.I(rho) - density_matrix.I(_rho)
