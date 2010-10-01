50074bd22de9b5e2013df2e7ed14dd5b7d8c5230
Balaji Rao <balaji@poornaprajna.org>
2010-10-01 12:32:40 +0530

    Fix line.py to take more sophisticated arguments
    Signed-off-by: Balaji Rao <balaji@poornaprajna.org>

--- random_walk/lib_based/line.py
+++ random_walk/lib_based/line.py
@@ -2,69 +2,127 @@
 
 # Quantum walk simulation
 
+from __future__ import print_function
+
 import qwalk_lib as qwalk
 import density_matrix as dm
-import discord
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+from optparse import OptionParser
 
-# Graphical output needed ?
-graph = True
+import discord
+import qmid
 
-# Print the probs ?
-print_probs = False
+def print_v(*args) :
+	global options
 
-step_spec = sys.argv[1].split(':')
+	if (options.verbose > 0) :
+		print (*args)
+	
 
-step_start = step_end = step_steps = 0
+usage = 'usage %prog : [options]'
+parser = OptionParser(usage = usage)
 
-if (len(step_spec) == 0 or len(step_spec) > 3) :
-	print "Bad args!"
-	exit()
-else :
-	if len(step_spec) >= 1 :
-		step_start = int(step_spec[0])
-		step_end = step_start
-		step_steps = 1
-		
-	if len(step_spec) >= 2 :
-		step_end = int(step_spec[1])
-		step_steps = 10
+parser.add_option('', '--qd', help = 'Calculate QD', action='store_true', dest = 'qd_needed')
+parser.add_option('', '--qmid', help = 'Calculate QMID', action='store_true', dest = 'qmid_needed')
 
-	if len(step_spec) == 3 :
-		step_steps = int(step_spec[2])
+parser.add_option('-N', '--node-range', type = 'int',  nargs = 3, help = 'Range of nodes to use', dest = 'node_range', metavar = "START END RESOLUTION")
+parser.add_option('-n', '--nodes', type = 'int',  default = 25, help = 'Number of nodes', dest = 'nodes', metavar = "NODES")
 
-	#Noise parameter
-	lamda = 0
+parser.add_option('-L', '--lamda-range', type = 'float',  nargs = 3, help = 'Range of lamdas to use for amp damping', dest = 'lamda_range', metavar = "START END RESOLUTION")
+parser.add_option('-l', '--lamda', type = 'float',  default = 0, help = 'Lamda param for amp damping', dest = 'lamda', metavar = "LAMDA")
+
+parser.add_option('-g', '--graphical', action = 'store_true', help = 'show graphical output', default = True, dest = 'graphical')
+parser.add_option('-v', '--verbose', help = 'verbose output ', type = int, default = 1, dest = 'verbose', metavar = "LEVEL")
+
+(options, args) = parser.parse_args()
+
+#Noise parameter
+lamda = 0
 
 #Coinspace is invariant
 coin_space = qwalk.CoinSpace()
 dim_A = len(coin_space)
 
-values = list()
-steps_space = range(step_start, step_end, step_steps)
-for steps in steps_space:
-	pos_list = range (-steps, steps + 1)
+output_list_dict = dict()
+
+if (options.qd_needed) :
+	output_list_dict['QD'] = list()
+
+if (options.qmid_needed) :
+	output_list_dict['QMID'] = list()
+
+_options = 0
+
+nodes_space = lamda_space = None
+
+if options.node_range is not None :
+	nodes_space = range(*options.node_range)
+	_options += 1
+else:
+	nodes_space = [options.nodes]
+
+
+print (options.lamda_range)
+if options.lamda_range is not None :
+	lamda_space = np.linspace(*options.lamda_range)
+	_options += 1
+else:
+	lamda_space = [options.lamda]
+
+if _options == 0 or _options != 1 :
+	parser.print_help()
+	sys.exit(1)
+
+for nodes in nodes_space:
+	pos_list = range (-nodes, nodes + 1)
+
 	pos_space = qwalk.PositionSpace(pos_list)
 	dim_B = len(pos_space)
 
-	E = [np.mat(np.empty((2 * (2 * steps + 1), 2 * (2 * steps + 1)))) for i in range (2)]
-	E[0] = np.kron(np.mat([[ np.sqrt(1 - lamda), 0], [0, 1]]), pos_space.I)
-	E[1] = np.kron(np.mat([[0, 0], [np.sqrt(lamda), 0]]), pos_space.I)
+	walk = qwalk.QWalk(pos_space, coin_space, circle = False)
 
-	walk = qwalk.QWalk(pos_space, coin_space)
+	for lamda in lamda_space:
+		E = [None for i in range(2)]
+		E[0] = np.kron(np.mat([[ np.sqrt(1 - lamda), 0], [0, 1]]), pos_space.I)
+		E[1] = np.kron(np.mat([[0, 0], [np.sqrt(lamda), 0]]), pos_space.I)
 
-	rho_CP = walk.do(steps, E)
-	_v = discord.T_min(rho_CP, dim_A, dim_B, qubit_subsys = 1) - dm.S_cond(rho_CP, (2, 1), dim_A, dim_B)
-	values.append(_v)
+		print_v ("Nodes : ", nodes, " Lambda = ", lamda)
 
-	print steps, " : ", _v
+		rho_CP = walk.do(nodes, E)
+		if (options.qd_needed) :
+			_qd = discord.T_min(rho_CP, dim_A, dim_B, qubit_subsys = 1) - dm.S_cond(rho_CP, (2, 1), dim_A, dim_B).real
+			output_list_dict['QD'].append(_qd)
+			print_v ("\tQD = ", _qd)
 
-#rho_P = dm.partial_trace(rho_CP, subsys = 1, dim_B = len(pos_space), dim_A = len(coin_space))
-#print np.diag(rho_P), np.trace(rho_P)
+		if (options.qmid_needed) :
+			_qm = qmid.qmid(rho_CP, dim_A, dim_B)
+			output_list_dict['QMID'].append(_qm)
+			print_v ("\tQMID = ", _qm)
+		
+		if (options.verbose > 1) :
+			rho_P = dm.partial_trace(rho_CP, subsys = 1, dim_B = len(pos_space), dim_A = len(coin_space))
+			print_v ("\tProbabilities : ", np.diag(rho_P), ". Sum = ", np.trace(rho_P))
 
-plt.plot(steps_space, values)
-plt.xlabel('Steps')
-plt.ylabel('D')
-plt.show()
+if (options.graphical) :
+	if options.node_range is not None :
+		_x = nodes_space
+		_label = 'Nodes'
+		_title = 'Walk on line with lamda = ' + str(options.lamda)
+	elif options.lamda_range is not None :
+		_x = lamda_space
+		_label = 'Noise Parameter'
+		_title = 'Walk on line with nodes = '+ str(options.nodes)
+
+	for k in output_list_dict.keys() :
+		_y = output_list_dict[k]
+		plt.plot(_x, _y, label = k)
+
+	plt.legend()
+	plt.xlabel(_label)
+	plt.title(_title)
+
+	plt.ylabel('Y')
+	plt.show()
+
